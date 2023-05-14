decb35bc171433aa71fc85a349edf58aa42106cf
leeport511 <leonardoandres.portillo@gmail.com>
2023-05-14 17:24:23 -0300

    eliminate dist from gitignore

---
+++ dist/assets/Team/index.js
@@ -0,0 +1,14 @@
+import anton from "./anton.png";
+import rodri from "./Rodri.png";
+import shanti from "./shanti.png";
+import tomis from "./Tomis.png";
+import zamfir from "./zamfir.png";
+
+
+export {
+    anton,
+    rodri,
+    shanti,
+    tomis,
+    zamfir,
+}
