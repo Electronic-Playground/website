18c17c41410b468e57093b48f5bb0043603e7a06
leeport511 <leonardoandres.portillo@gmail.com>
2023-05-15 19:56:17 -0300

    changue gradient in gamecard

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const GameCard = ({ game }) => (
-    <div className="bg-gamecard flex w-[350px] h-[250px] cursor-pointer flex-col rounded-xl px-6 py-3 cardTransition bg-slate-50  hover:-translate-y-3 hover:shadow-md">
+    <div className="bg-gamecard flex w-[350px] h-[250px] cursor-pointer flex-col rounded-xl px-6 py-3 cardTransition bg-slate-50  hover:-translate-y-3 hover:shadow-xl outline-primaryStar outline outline-1">
         <div className="mb-4 flex m-auto gap-x-8 items-center">
             <img src={game.logo} alt={game.title} className="w-[60px] rounded-full outline outline-2 outline-offset-1 outline-bg-main-color" />
             <h3 className="font-akzidenz text-lg font-bold uppercase text-bg-main-color">
