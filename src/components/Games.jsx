0bd8d463a0c33dae638e5b0df627cade2d629530
leeport511 <leonardoandres.portillo@gmail.com>
2023-05-19 15:26:19 -0300

    change rotate in specific games

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -3,39 +3,52 @@ import GameCard from "./GameCard";
 import { games } from "../constants";
 
 const Games = () => {
-
-    const [highlightGame, setHighlightGame] = useState(null)
+    const [highlightGame, setHighlightGame] = useState(null);
 
     const handleHover = (id) => {
-      
-        const game = games.find(game => game.id === id);
+        const game = games.find((game) => game.id === id);
         if (game) {
             setHighlightGame(game);
         }
-    }
-
-    
+    };
 
     return (
         <section
             id="games"
             className="relative my-12 flex flex-1 items-center justify-center lg:justify-end"
         >
-            <div className="cell-box-shadow  cellphoneImg absolute -left-20 top-44 hidden max-w-[455px] rounded-2xl border-2 lg:flex">
-                <img
-                    src="../../assets/Icons/cellphone-01.png"
-                    alt="cellphone"
-                    className="cell-box-shadow"
-                />
-                {
-                  highlightGame &&      
+            {highlightGame.title === "Dawnblade" ||
+            highlightGame.title === "The Otherside" ? (
+                <div className="cell-box-shadow  cellphoneImg absolute transition-all -left-28gi top-44 hidden max-w-[455px] rotate-90 rounded-2xl border-2 lg:flex">
                     <img
-                        src={highlightGame ? highlightGame.image : null}
-                        alt="random"
-                        className="absolute left-8 top-7 rounded-3xl"
+                        src="../../assets/Icons/cellphone-01.png"
+                        alt="cellphone"
+                        className="cell-box-shadow"
                     />
-                }
-            </div>
+                    {highlightGame && (
+                        <img
+                            src={highlightGame ? highlightGame.image : null}
+                            alt="random"
+                            className={`absolute left-8 top-7 rounded-3xl`}
+                        />
+                    )}
+                </div>
+            ) : (
+                <div className="cell-box-shadow  cellphoneImg absolute transition-all -left-20 top-44 hidden max-w-[455px] rounded-2xl border-2 lg:flex">
+                    <img
+                        src="../../assets/Icons/cellphone-01.png"
+                        alt="cellphone"
+                        className="cell-box-shadow"
+                    />
+                    {highlightGame && (
+                        <img
+                            src={highlightGame ? highlightGame.image : null}
+                            alt="random"
+                            className={`absolute left-8 top-7 rounded-3xl`}
+                        />
+                    )}
+                </div>
+            )}
             <div className="flex flex-col justify-evenly lg:w-[70%] slg:w-[56.7%]  ">
                 <div className="">
                     <h2 className=" my-12 text-center font-akzidenz-Ext text-5xl font-normal tracking-[0.14em] text-bg-main-color">
@@ -53,7 +66,7 @@ const Games = () => {
                             }`}
                             onMouseOver={() => handleHover(game.id)}
                         >
-                            <GameCard game={game}/>
+                            <GameCard game={game} />
                         </div>
                     ))}
                 </div>
@@ -63,3 +76,25 @@ const Games = () => {
 };
 
 export default Games;
+
+
+
+{/* <div className="cell-box-shadow  cellphoneImg absolute -left-20 top-44 hidden max-w-[455px] rounded-2xl border-2 lg:flex">
+                <img
+                    src="../../assets/Icons/cellphone-01.png"
+                    alt="cellphone"
+                    className="cell-box-shadow"
+                />
+                    {highlightGame && (
+                        <img
+                            src={highlightGame ? highlightGame.image : null}
+                            alt="random"
+                            className={`absolute left-8 top-7 rounded-3xl ${
+                                highlightGame.title === "Dawnblade" ||
+                                highlightGame.title === "The Otherside"
+                                    ? "horizontal-img"
+                                    : ""
+                            }`}
+                        />
+                    )}
+                </div> */}
