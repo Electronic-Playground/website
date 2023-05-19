246e590edf5a94055a1aea7cc6dee9babd46e645
leeport511 <leonardoandres.portillo@gmail.com>
2023-05-19 14:19:46 -0300

    make changes in dist folder, made a build

--- src/components/ClientCard.jsx
+++ src/components/ClientCard.jsx
@@ -2,14 +2,12 @@ import React from "react";
 
 const ClientCard = ({ client, index }) => (
     <div
-        className={`cardTransition flex w-[200px] items-center overflow-hidden rounded-2xl outline outline-2 outline-primaryBorder hover:-translate-y-3 ${
-            client.name === "Axios Games" && "w-[129px]"
-        } hover:shadow-lg hover:shadow-primaryStar`}
+        className={`cardTransition flex w-[265px] items-center overflow-hidden rounded-2xl outline outline-2 outline-primaryBorder hover:-translate-y-3 hover:shadow-lg hover:shadow-primaryStar`}
     >
         <img
             src={client.image}
             alt={client.name}
-            className={`text-bg-main-color w-[100%] h-auto`}
+            className={`h-auto w-[100%] text-bg-main-color`}
         />
     </div>
 );
